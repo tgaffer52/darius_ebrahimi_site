8f4c277781454dbbef0b008357c135e7469158c0
Trevor Gaffney <tgaffer@gmail.com>
2023-10-16 19:19:14 +0900

    Add scroll effect in js

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,16 +1,29 @@
-import React from "react";
+"use client";
+
 import Image from "next/image";
 import "@app/globals.css";
 import styles from "./Nav.module.scss";
-import Link from "next/link";
+// import Link from "next/link";
+import React, { useEffect } from "react";
+import {
+  Link,
+  Button,
+  Element,
+  Events,
+  animateScroll as scroll,
+  scrollSpy,
+} from "react-scroll";
 
 const Nav = () => {
+  const scrollToTop = () => {
+    window.scroll(top);
+  };
   return (
     <section>
       <nav className="flex w-full fixed top-0">
-        <Link href="/#top" passHref>
+        <button type="button" onClick={scrollToTop}>
           <img src="/assets/logo.png" className={styles.logo} alt="logo" />
-        </Link>
+        </button>
         <div className={styles.title}>
           <h1 className="text-center">
             DAR{" "}
@@ -22,27 +35,24 @@ const Nav = () => {
           <h4 className="text-center">Fantasy Author</h4>
         </div>
         <div className="nav-links flex flex-col bg-gray-700/75 pb-0">
-          <Link
-            passHref
+          <a
             href="/#detective-death"
             className="text-center text-white hover:bg-red-700/75 rounded-md px-3 py-2 text-sm font-medium"
           >
             BOOKS
-          </Link>
-          <Link
-            passHref
+          </a>
+          <a
             href="/#about"
             className="text-center text-white hover:bg-red-700/75 rounded-md px-3 py-2 text-sm font-medium"
           >
             ABOUT
-          </Link>
-          <Link
-            passHref
+          </a>
+          <a
             href="/#contact"
             className="text-center text-white hover:bg-red-700/75 rounded-md px-3 py-2 text-sm font-medium"
           >
             CONTACT
-          </Link>
+          </a>
         </div>
       </nav>
       <div className="h-28"></div>
