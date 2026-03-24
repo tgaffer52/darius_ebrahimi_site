82b5052cccb9341510b15871177bf05a1c15480c
Trevor Gaffney <tgaffer@gmail.com>
2026-03-24 21:40:34 +0900

    Footer fix

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,8 +2,10 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-800 w-full h-12 flex flex-wrap content-center pl-3 fixed bottom-0">
-      <h4 className="text-white">Mythical Bandit Publishing 2023</h4>
+    <footer className="bg-gray-800 w-full h-12 flex flex-wrap content-center pl-3 mt-10">
+      <h4 className="text-white">
+        © 2026 Mythical Bandit Publishing. All rights reserved.
+      </h4>
     </footer>
   );
 };
